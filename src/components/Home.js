a58eb8d293f43dcf7e06ea4a31ded648920a5ec6
nhquang <quangpersie1@gmail.com>
2021-07-10 13:25:21 -0400

    deploying to heroku

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     const [tvshows, settvshows] = useState([]);
     useEffect(() => {
         const fetchMovies = async () =>{
-            const data = await fetch("http://localhost:3000/movies");
+            const data = await fetch("http://localhost:3001/movies");
             const parsed = await data.json();
             const temp = [];
             for(let i = 0; i < 8; i++)
@@ -17,7 +17,7 @@ const Home = () => {
             setmovies(prev => temp);
         }
         const fetchTVShows = async() =>{
-            const data = await fetch("http://localhost:3000/tvShows");
+            const data = await fetch("http://localhost:3001/tvShows");
             const parsed = await data.json();
             const temp = [];
             for(let i = 0; i < 8; i++)
@@ -25,7 +25,7 @@ const Home = () => {
             settvshows(prev => temp);
         }
         const fetchSlideShowItems = async() =>{
-            const data = await fetch("http://localhost:3000/banners");
+            const data = await fetch("http://localhost:3001/banners");
             const parsed = await data.json();
             setslideShowItems(prev => parsed);
         }
